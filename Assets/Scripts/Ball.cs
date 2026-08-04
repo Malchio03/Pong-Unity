f40915adf25ed8a2272c99114306673fa49b82cf
Riccardo Malchiodi <115894161+Malchio03@users.noreply.github.com>
2026-08-04 14:36:43 +0200

    Resetting the ball

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,8 +5,10 @@ public class Ball : MonoBehaviour
     public Rigidbody2D rb;
     public float moveSpeed;
     public float maxInitialAngle = 0.67f;
-    public float startX = 0f;
     public float maxStartY = 4f;
+
+    private float startX = 0f;
+
     void Start()
     {
        InitialPush();   
@@ -15,8 +17,18 @@ public class Ball : MonoBehaviour
     private void InitialPush()
     {
         Vector2 dir = Vector2.left * moveSpeed;     // Start moving the ball to the left
+        if(Random.value < 0.5f)
+            dir = Vector2.right * moveSpeed;        // Randomly change direction to the right
+
         dir.y = Random.Range(-maxInitialAngle, maxInitialAngle);
         rb.linearVelocity = dir;
+    }
+
+    private void ResetBall()
+    {
+        float posY = Random.Range(-maxStartY, maxStartY);
+        Vector2 position = new Vector2(startX, posY);
+        transform.position = position;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -24,7 +36,8 @@ public class Ball : MonoBehaviour
         ScoreZone scorezone = collision.GetComponent<ScoreZone>();
         if(scorezone != null)
         {
-            Debug.Log("todo");
+            ResetBall();
+            InitialPush();
         }
     }
 }
